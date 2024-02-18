05e4d2da49e7eb0d3757532ab71247715edbc410
snrufomechanic <anil.oz@icloud.com>
2024-02-18 15:20:51 +0300

    inital

--- src/pages/api/createMap.js
+++ src/pages/api/createMap.js
@@ -1,4 +1,99 @@
+import fetch from "node-fetch";
+import axios from "axios";
+const canvas = require("canvas");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  const zip = function (_points) {
+    const result = [];
+    for (let i = 0; i < _points.length; i++) {
+      result.push([_points[i].x, _points[i].y]);
+    }
+    return result;
+  };
+
+  const NOMINATIM_URL = "https://nominatim.openstreetmap.org/search.php";
+  const OVERPASS_API_URL = "https://lz4.overpass-api.de/api/interpreter";
+
+  const country = req.body.country;
+  const city = req.body.city;
+  const district = req.body.district;
+
+  const location = `${country}, ${city}, ${district}`;
+  try {
+    var nominatimResponse = await fetch(
+      NOMINATIM_URL +
+        "?q=" +
+        country +
+        "+" +
+        city +
+        "+" +
+        district +
+        "&format=json"
+    );
+
+    const data = await nominatimResponse.json();
+
+    if (data) {
+      const latitude = parseFloat(data[0].lat);
+      const longitude = parseFloat(data[0].lon);
+
+      console.log("latitude:", latitude);
+      console.log("longitude:", longitude);
+
+      const overpassQuery = `[out:json];way["highway"](${latitude - 0.05},${
+        longitude - 0.05
+      },${latitude + 0.05},${longitude + 0.05});(._;>;);out;`;
+
+      const overpassResponse = await axios.get(OVERPASS_API_URL, {
+        params: { data: overpassQuery },
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const overpassData = overpassResponse.data;
+
+      console.log("overpass data:", overpassData);
+
+      // önce burada görüntü oluştmayı denedim başaramadım ^^
+
+      // const outCanvas = new canvas.Canvas(600, 600);
+      // const outCtx = outCanvas.getContext("2d");
+
+      // for (const element of overpassData.elements) {
+      //   if (element.type === "way") {
+      //     const points = [];
+      //     for (const nodeId of element.nodes) {
+      //       const node = overpassData.elements.find(
+      //         (e) => e.type === "node" && e.id === nodeId
+      //       );
+      //       if (node) {
+      //         points.push([parseFloat(node.lon)], parseFloat(node.lat));
+      //       }
+      //     }
+      //     if (points.length && outCtx) {
+      //       console.log(points);
+      //       const [x, y] = zip(points);
+      //       console.log("x,y zipped");
+      //       console.log(x);
+      //       console.log(y);
+      //       outCtx.strokeStyle = "white";
+      //       outCtx.beginPath();
+      //       outCtx.moveTo(x[0], y[0]);
+      //       for (let i = 1; i < x.length; i++) {
+      //         outCtx.lineTo(x[i], y[i]);
+      //       }
+      //       outCtx.stroke();
+      //     }
+      //   }
+      // }
+      res.setHeader("Content-Type", "image/png");
+      res.send(overpassData);
+    } else {
+      res.status(400).json({ message: "Invalid location" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    console.log("finally");
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,119 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
 
 export default function Home() {
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [district, setDistrict] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("/api/createMap", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ country, city, district }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("data:", data);
+      const map = document.getElementById("map");
+      // yeniden kullanımda görüntüler üst üste gelmesin diye temizlik
+      while (map.firstChild) {
+        map.removeChild(map.firstChild);
+      }
+
+      let minLon = Infinity,
+        maxLon = -Infinity,
+        minLat = Infinity,
+        maxLat = -Infinity;
+
+      data.elements.forEach((element) => {
+        if (element.type === "node") {
+          minLon = Math.min(minLon, element.lon);
+          maxLon = Math.max(maxLon, element.lon);
+          minLat = Math.min(minLat, element.lat);
+          maxLat = Math.max(maxLat, element.lat);
+        }
+      });
+
+      const lonRange = maxLon - minLon;
+      const latRange = maxLat - minLat;
+      const svgWidth = map.clientWidth;
+      const svgHeight = map.clientHeight;
+      const lonScale = svgWidth / lonRange;
+      const latScale = svgHeight / latRange;
+
+      for (const element of data.elements) {
+        if (element.type === "node") {
+          const scaledLon = (element.lon - minLon) * lonScale;
+          const scaledLat = (element.lat - minLat) * latScale;
+
+          const circle = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+          );
+
+          circle.setAttribute("cx", scaledLon);
+          circle.setAttribute("cy", scaledLat);
+          circle.setAttribute("r", "2");
+          circle.setAttribute("fill", "black");
+
+          map.appendChild(circle);
+        }
+      }
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <form>
-        <label className="m-2">
-          <span>Country:</span>
-        </label>
-        <input type="text" name="country" />
-
-      </form>
-
-
-
+    <main>
+      <div
+        className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+      >
+        <form onSubmit={handleSubmit} autoComplete="false">
+          <label htmlFor="country" className="m-2">
+            Country:
+          </label>
+          <input
+            type="text"
+            name="country"
+            id="country"
+            values={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            values={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <label htmlFor="district">District:</label>
+          <input
+            type="text"
+            name="district"
+            id="district"
+            values={district}
+            onChange={(e) => setDistrict(e.target.value)}
+          />
+          <button
+            type="submit"
+            variant="contained"
+            color="success"
+            className="m-2"
+          >
+            Submit
+          </button>
+        </form>
+        <svg id="map" width="100%" height="100%" viewBox="0 0 1000 1000"></svg>
+      </div>
     </main>
   );
 }
